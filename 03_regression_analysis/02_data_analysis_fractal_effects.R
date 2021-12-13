f2c3e91c040f9fef67d91d39c8f197eb43ac3f59
shaharlab <shaharlab@shaharlab.com>
2021-12-13 17:08:25 +0000

    glmer_update

--- 03_regression_analysis/02_data_analysis_fractal_effects.R
+++ 03_regression_analysis/02_data_analysis_fractal_effects.R
@@ -8,16 +8,21 @@ library(tidyr)
 library(lme4)
 library(raincloudplots)
 load('data/analysis_data/tab.Rdata')
-
+load('data/analysis_data/tab_unch.Rdata')
 
 ##### Counterfactual stay effect --------------------------------------------------------
 #means
-tab%>%
-  filter(reoffer_ch==F,reoffer_unch==T)%>%
+tab_unch%>%
   group_by(subj,cond,rw_oneback)%>%
   summarise(pStay_unch=mean(stay_frc_unch))%>%
   pivot_wider(names_from=c(cond,rw_oneback),values_from  =pStay_unch)%>%
   colMeans()
+
+#glmer
+
+m<-glmer(stay_frc_unch ~ 1+rw_oneback + (1+rw_oneback*cond|prolific_id), 
+         data = tab_unch, 
+         family = binomial, control = glmerControl(optimizer = "bobyqa"),nAGQ = 1)
 
 # ggplot
 df<-
