82feafa49ff07bee4d04cce2dbb38195f0c55855
NitzanShahar <68427888+NitzanShahar@users.noreply.github.com>
2022-01-04 19:18:51 +0200

    make data subfolder

--- model_approach_avoid/examine_elpd.R
+++
@@ -1,7 +0,0 @@
-#This code plot recovered parameters against the true parameters
-
-rm(list=ls())
-myfolder='./model_approach_avoid/'
-#--------------------------------------------------------------------------------------------------------
-
-load(paste0(myfolder,'data/modelfit_like_per_trial.rdata'))
